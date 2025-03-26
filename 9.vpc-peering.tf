dab57b2f2872bcc8503540f50641e60bf6ee43da
csarat424 <csarat424@gmail.com>
2025-03-26 08:02:28 -0400

    changed tfvars and peering files

--- 9.vpc-peering.tf
+++ 9.vpc-peering.tf
@@ -3,7 +3,7 @@ data "aws_vpc" "ansible_vpc" {
 }
 
 data "aws_route_table" "ansible_vpc_rt" {
-  vpc_id = data.aws_vpc.ansible_vpc.id
+  subnet_id = "subnet-085df45a822371ae8"
 }
 #If subnet_id giving errors use route table id as below
 #route_table_id = data.aws_route_table.ansible_vpc_rt.id
@@ -33,7 +33,7 @@ resource "aws_route" "peering-to-ansible-vpc" {
 
 resource "aws_route" "peering-from-ansible-vpc" {
   route_table_id            = data.aws_route_table.ansible_vpc_rt.id
-  destination_cidr_block    = "172.31.0.0/16"
+  destination_cidr_block    = "10.37.0.0/16"
   vpc_peering_connection_id = aws_vpc_peering_connection.ansible-vpc-peering.id
   #depends_on                = [aws_route_table.terraform-public]
 }
